d5108199991b0865bf9adf7613649f57593c0a47
missmimia <Zhutou14/>
2018-06-08 20:44:43 +0800

    'fix:链接服务器成功，成功获取数据

---
+++ server/controllers/product.js
@@ -0,0 +1,13 @@
+// https://classroom.udacity.com/courses/ud666-cn-2/lessons/8ee6dbed-7b9b-46fc-a127-c2044aec7378/concepts/c233860f-5283-424a-ab68-488934b93a5d
+const DB = require('../utils/db.js')
+
+module.exports = {
+  /**
+   * 拉取商品列表
+   * 
+   */
+
+  list: async ctx => {
+    ctx.state.data = await DB.query("SELECT * FROM product;")
+  } // 为什么这里要赋值给 state.data
+}
